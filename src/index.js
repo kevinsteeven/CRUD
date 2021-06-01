c88956f78c4d2aedf5c76ea3bf7fab95f4576d70
Kevin Steeven <kevingarcs10@gmail.com>
2021-05-31 21:12:18 -0500

    Importacion de mysql

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path')
 const app=express();
+const morgan = require('morgan');
+const mysql= require('mysql');
+const myConnection=require('express-myconnection')
 //Configuración
 app.set('port', process.env.PORTǀǀ3000);
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'views'))
+app.set('views',path.join(__dirname,'views'));
+//middlewares
+app.use(morgan('dev'));
 app.listen(app.get('port'),()=>{
     console.log('Server on port 3000');
 })
